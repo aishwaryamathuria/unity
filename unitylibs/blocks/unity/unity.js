0c336efb0e2b73b1894c7eb7b8acf1c147a11671
mathuria <mathuria@adobe.com>
2024-06-27 16:14:15 +0530

    unity block poc

--- unitylibs/blocks/unity/unity.js
+++ unitylibs/blocks/unity/unity.js
@@ -2,5 +2,4 @@ export default async function init(el) {
   const unitylib = 'https://blockpoc--unity--adobecom.hlx.page'
   const { default: init } = await import(`${unitylib}/unitylibs/core/workflow/workflow.js`);
   init(el);
-
 }
